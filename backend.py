7ac27d838b1a5899de8f375583947106be68eccf
ardit <fyuskas@gmail.com>
2023-08-23 13:01:39 +0400

    Complete app

--- backend.py
+++ backend.py
@@ -1,10 +1,17 @@
 import requests
-API_KEY="333145fa34d0e9742aeb51446ff958dd"
-def get_data(place,forecast_days=None,kind=None,):
-    url = f"http://api.openweathermap.org/data/2.5/forecast?id={place}&appid={API_KEY}"
-    response=requests.get(url)
-    data=response.json()
-    return data
+
+API_KEY = "141710af2113bab9f55ef73e1bcd33d5"
+
+
+def get_data(place, forecast_days=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
+
 
 if __name__=="__main__":
-    print(get_data(place="Tokyo"))
+    print(get_data(place="Tokyo", forecast_days=3))
